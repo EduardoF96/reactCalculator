0e891be26f4465cd06a357580c6b98b5618f8ff2
Eduardo F G <eduardo.floresgomez96@gmail.com>
2022-03-15 18:01:42 -0600

    Primera versión de calculadora

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -9,14 +9,7 @@ function App() {
         <p>
           ojo <code>Empezamos con la calculadora</code> 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Calculadora V1
       </header>
     </div>
   );
